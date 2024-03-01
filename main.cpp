c7f9902436972e31e63b0335a0c476f4e7f4c3d5
connorhartwell <hartwellconnor@gmail.com>
2024-03-01 12:33:08 +0000

    modified m_search to return vector

--- main.cpp
+++ main.cpp
@@ -7,20 +7,31 @@
 #include <cstring>
 #include <fcntl.h>
 #include <time.h>
+#include <chrono>
+#include <thread>
+#include <vector>
+#include "msearch.h"
 
-using std::cout;
-using std::cerr;
+using namespace std;
 
 int main() {
+    vector<string> deviceList = msearch::search_for_devices();
+    for(int i = 0; i < deviceList.size(); i++) {
+        
+    }
+}
+
+vector<string> msearch::search_for_devices() {
     int sock;
     struct sockaddr_in addr;
     int port = 1900;
-    std::string searchRequest =
+    vector<string> results;
+    string searchRequest =
         "M-SEARCH * HTTP/1.1\r\n"
         "HOST: 239.255.255.250:1900\r\n"
         "MAN: \"ssdp:discover\"\r\n"
-        "MX: 1\r\n" // Maximum wait time in seconds
-        "ST: upnp:rootdevice\r\n" // Search target
+        "MX: 1\r\n"
+        "ST: upnp:rootdevice\r\n"
         "\r\n";
     char buffer[2048];
     struct sockaddr_in recv_addr;
@@ -30,8 +41,13 @@ int main() {
     sock = socket(AF_INET, SOCK_DGRAM, 0);
     if (sock < 0) {
         cerr << "Failed to create socket" << std::endl;
-        return -1;
+        return results;
     }
+
+    struct timeval read_timeout;
+    read_timeout.tv_sec = 0;
+    read_timeout.tv_usec = 100;
+    setsockopt(sock, SOL_SOCKET, SO_RCVTIMEO, &read_timeout, sizeof read_timeout);
 
     memset(&addr, 0, sizeof(addr));
     addr.sin_family = AF_INET;
@@ -42,29 +58,27 @@ int main() {
                (struct sockaddr *)&addr, sizeof(addr)) < 0) {
         cerr << "Failed to send message" << std::endl;
         close(sock);
-        return -1;
+        return results;
     }
 
-    cout << "SSDP M-SEARCH request sent." << std::endl;
-    cout << "Listening for responses..." << std::endl;
+    cout << "SSDP M-SEARCH request sent." << endl;
+    cout << "Listening for responses..." << endl;
 
     int initial_time = time(NULL);
-    while (true) {
+    while (time(NULL) - initial_time < 3) {
         ssize_t recv_len = recvfrom(sock, buffer, sizeof(buffer) - 1, 0,
                                     (struct sockaddr *)&recv_addr, &recv_addr_len);
         if (recv_len > 0) {
             buffer[recv_len] = '\0';
-            cout << "Received response:\n" << buffer << std::endl;
+            cout << "Received response:\n" << buffer  << endl;
+            results.push_back(buffer);
         }
-
-        if((time(NULL) - initial_time) > 2 ) {
-            break;
-        }
-        usleep(100000); // Sleep for 100 milliseconds
+        this_thread::sleep_for(chrono::milliseconds(100));
     }
 
-    // Close the socket
+    cout << "msearch finished" << endl;
+    cout << results.size();
     close(sock);
 
-    return 0;
+    return results;
 }
